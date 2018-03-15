3d611355a99f19885e402a9483e9c6014b690756
Sergey Zolotarev <sryze@yandex.com>
2018-03-16 02:56:14 +0600

    Another useless commit

--- tools.js
+++ tools.js
@@ -1,3 +1,10 @@
+if (!String.prototype.trim) {
+    String.prototype.trim = function() {
+        return this.replace(/^\s+/, '').replace(/\s+$/, '');
+    };
+}
+
+var params = parseURLParams(location.hash.substring(1));
 var converters = {
     'to-uppercase': function(input) {
         return input.toUpperCase();
@@ -7,15 +14,35 @@ var converters = {
     }
 };
 
+function parseURLParams(query) {
+    var params = {};
+    var pairs = query.split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var nameValue = pairs[i].split('=');
+        var name = (decodeURIComponent(nameValue[0]) || '').trim();
+        var value = decodeURIComponent(nameValue[1]);
+        if (name && name.length > 0) {
+            params[name] = value;
+        }
+    }
+    return params;
+}
+
 addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('convert-button');
+    var inputArea = document.getElementById('input-area');
+    inputArea.value = params['input'];
 
-    button.addEventListener('click', function(e) {
-        var inputArea = document.getElementById('input-area');
-        var outputArea = document.getElementById('output-area');
+    var outputArea = document.getElementById('output-area');
+    var convertButton = document.getElementById('convert-button');
 
-        var select = document.getElementById('converter-select');
-        var converterOption = select.selectedOptions[0];
+    var toolSelect = document.getElementById('converter-select');
+    var initialTool = params['tool'];
+    if (converters.keys().contains(initialTool)) {
+        converterSelect.value = initialTool;
+    }
+
+    convertButton.addEventListener('click', function(e) {
+        var converterOption = toolSelect.selectedOptions[0];
         if (!converterOption || converterOption.value === '_') {
             return;
         }
